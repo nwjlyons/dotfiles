83a3c3da6c48e7e3ff14d2f41477e8f8a7e5561e
Neil Lyons <nwjlyons@googlemail.com>
2014-09-03 11:52:16 +0900

    Updated openbox keybindings.

--- install.sh
+++ install.sh
@@ -2,10 +2,14 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# Symlinks
 ln -sf "$DIR/.bashrc" "$HOME/.bashrc"
 ln -sf "$DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sf "$DIR/.hgrc" "$HOME/.hgrc"
 ln -sf "$DIR/.vimrc" "$HOME/.vimrc"
 ln -sf "$DIR/.conkyrc" "$HOME/.conkyrc"
-# Restart conky
+ln -sf "$DIR/openbox/rc.xml" "$HOME/.config/openbox/rc.xml"
+
+# Restarts
 conkywonky
+openbox --restart
